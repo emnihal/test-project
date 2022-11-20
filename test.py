c324022cb7965681b94f66e5ed4f98ab341c354b
Em Nihal <nihalcyberdude2@gmail.com>
2022-11-20 16:19:04 +0530

    test

--- test.py
+++ test.py
@@ -3,4 +3,4 @@ aws_secret="AKIAIMNOJVGFDXXXE4UI"
 aws_access_key_id = AKIAYVP4CIPPFFFXD5QE
 aws_secret_access_key = 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
 output = json
-region = us-east-2
+region = us-east-1
