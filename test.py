c677ea931027ffe877ca51a98702379cf27bd7c5
Em Nihal <nihalcyberdude2@gmail.com>
2022-11-20 15:18:27 +0530

    Test

--- test.py
+++ test.py
@@ -1,2 +1,6 @@
 aws_secret="AKIAIMNOJVGFDXXXE4UI"
-test
+[default]
+aws_access_key_id = AKIAYVP4CIPPFFFXD5QE
+aws_secret_access_key = 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
+output = json
+region = us-east-2
